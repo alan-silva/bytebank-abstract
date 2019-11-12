ac3510f90ef99430eea7a74a1c01e1fc462cf65c
alan.cardoso <alan.cardoso@ARGO0019.Argo.Local>
2019-11-12 16:10:53 -0300

    Test Classe Abstract

---
+++ bytebank-abstract/src/ContaPoupanca.java
@@ -0,0 +1,19 @@
+
+public class ContaPoupanca extends Conta {
+	
+	public ContaPoupanca(int agencia, int numero) {
+		
+		super(agencia,numero);//
+		
+		
+	}
+
+	@Override
+	public void deposita(double valor) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+
+}
